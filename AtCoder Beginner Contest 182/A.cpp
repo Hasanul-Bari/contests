919e99fc0bc43afd00dc6d01ccbb6e03446d3001
Hasanul Bari <hasanul.bari.hasan96@gmail.com>
2020-11-09 22:28:05 +0600

    Create A.cpp

---
+++ AtCoder Beginner Contest 182/A.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+#define faster  ios :: sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define ll long long
+#define ull unsigned long long
+#define pb push_back
+const double PI = acos(-1.0);
+using namespace std;
+
+int main()
+{
+    faster
+    int a,b;
+    cin>>a>>b;
+
+    int r=(2*a)+100;
+
+    cout<<r-b<<endl;
+
+
+
+
+    return 0;
+}
