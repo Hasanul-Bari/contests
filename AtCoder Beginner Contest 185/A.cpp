b4492b379a33669490025abd0fc9525973373d29
Hasanul Bari <hasanul.bari.hasan96@gmail.com>
2020-12-16 23:01:18 +0600

    Create A.cpp

---
+++ AtCoder Beginner Contest 185/A.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+#define faster  ios :: sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define ll long long
+#define ull unsigned long long
+#define pb push_back
+const double PI = acos(-1.0);
+using namespace std;
+
+int main()
+{
+    faster
+
+    int a,b,c,d;
+    cin>>a>>b>>c>>d;
+
+    cout<<min(a,min(b,min(c,d)))<<endl;
+
+
+
+
+    return 0;
+}
