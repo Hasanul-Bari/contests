ac996a6b76beb8562e576ddce022aafb0df171ce
Hasanul Bari <hasanul.bari.hasan96@gmail.com>
2021-01-27 22:30:57 +0600

    Create A.cpp

---
+++ AtCoder Beginner Contest 189/A.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+#define faster  ios :: sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define ll long long
+#define ull unsigned long long
+#define pb push_back
+const double PI = acos(-1.0);
+using namespace std;
+
+int main()
+{
+    faster
+
+    string s;
+    cin>>s;
+
+    if(s[0]==s[1] && s[1]==s[2])
+    cout<<"Won"<<endl;
+    else
+    cout<<"Lost"<<endl;
+
+
+
+
+    return 0;
+}
