9c589f33747735b04c73427d78319df20b69c825
Hasanul Bari <hasanul.bari.hasan96@gmail.com>
2021-06-25 00:34:56 +0600

    Create A. Add Candies.cpp

---
+++ Codeforces Round #683 (Div. 2, by Meet IT)/A. Add Candies.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+#define faster  ios :: sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define ll long long
+#define ull unsigned long long
+#define pb push_back
+const double PI = acos(-1.0);
+using namespace std;
+
+void solve(int tc)
+{
+    int n;
+    cin>>n;
+
+    cout<<n<<endl;
+    for(int i=1; i<=n; i++)
+    {
+        cout<<i<<" ";
+    }
+    cout<<endl;
+
+}
+
+
+int main()
+{
+    faster
+
+    int t;
+    cin>>t;
+
+    //solve(1);
+    for(int tc=1; tc<=t; tc++)solve(tc);
+
+
+    return 0;
+}
+
