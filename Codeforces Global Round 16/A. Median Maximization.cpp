d205c01363369126bc49723eda76cdec2bedb9ff
Hasanul Bari <hasanul.bari.hasan96@gmail.com>
2021-10-27 22:43:52 +0600

    Create A. Median Maximization.cpp

---
+++ Codeforces Global Round 16/A. Median Maximization.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+#define faster  ios :: sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define ll long long
+#define ull unsigned long long
+#define pb push_back
+const double PI = acos(-1.0);
+using namespace std;
+
+void solve(int tc)
+{
+    int n,s;
+    cin>>n>>s;
+
+    int m=(n/2)+1;
+
+
+    //cout<<s<<" ** "<<m<<endl;
+
+    cout<<s/m<<endl;
+
+}
+
+
+int main()
+{
+    faster
+
+    int t; cin>>t; for(int tc=1; tc<=t; tc++)solve(tc);
+
+    //solve(1);
+
+
+
+    return 0;
+}
+
+
